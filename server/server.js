967ab3b368e0ce0f84083d1567294a3c74951dd0
Christina Woodhams <cwoodhams89@gmail.com>
2022-11-05 20:28:21 -0400

    Added a form to input names and connected Rollbar.

--- server/server.js
+++ server/server.js
@@ -6,8 +6,59 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "../public")));
-app.use("images", express.static(path.join(__dirname, "../images")));
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'f1ce2b237ca44f6498f81e005cf038d9',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
+const pets = ['Mocha', 'Pedro']
+
+app.get('/', (req, res) => {
+    rollbar.info("HTML served successfully.")
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/pets', (req, res) => {
+    rollbar.info("Pets have been loaded.")
+    res.status(200).send(pets)
+})
+
+app.post('/api/pets', (req, res) => {
+   let {name} = req.body
+
+   const index = pets.findIndex(pet => {
+       return pet === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           rollbar.log("Pet added successfully.", {author: "Lily", type: "manual entry"});
+           pets.push(name)
+           res.status(200).send(pets)
+       } else if (name === ''){
+           rollbar.error("No name provided");
+           res.status(400).send('You must enter a name.')
+       } else {
+           rollbar.error("Pet already exists.")
+           res.status(400).send('That pet already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/pets/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    pets.splice(targetIndex, 1)
+    res.status(200).send(pets)
+})
+
+app.use(rollbar.errorHandler());
 
 const port = process.env.PORT || 4005;
 
